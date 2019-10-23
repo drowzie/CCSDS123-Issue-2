a5b69a1f1ff06d7c12cc873393741b12a6407eff
Christofer Boothby <chrisboo@stud.ntnu.no>
2019-10-23 18:29:26 +0200

    Writing bits now works

--- src/encoder/encoder.c
+++ src/encoder/encoder.c
@@ -6,17 +6,20 @@ int encode(unsigned int * counter, unsigned int * accumulator, int x, int y, int
     return 0;
 }
 
+/*
+    write numBits of writeValue into the file stream. numWrittenBits and storedValue 
+*/
 
-void bitStream_store(unsigned char * compressed_stream, unsigned int * written_bytes,
-                    unsigned int * written_bits, unsigned int num_bits_to_write, unsigned int bits_to_write, FILE * stream){
-    int i = 0;
-    for(i = num_bits_to_write - 1; i >= 0; i--){
-        compressed_stream[*written_bytes] |= ((bits_to_write >> i) & 0x1) << (7 - *written_bits);
-        (*written_bits)++;
-        if(*written_bits >= 8){
-            *written_bits = 0;
-            fwrite(compressed_stream[*written_bytes], 1, sizeof(char), stream);
-            (*written_bytes)++;
+unsigned char storedValue = 0;
+
+void writeValue(unsigned int writeValue, unsigned int numBits, unsigned int * numWrittenBits, FILE * stream){
+    for(int i = numBits - 1; i >= 0; i--){
+        storedValue |= ((writeValue >> i) & 0x1) << (7 - *numWrittenBits);
+        (*numWrittenBits)++;
+        if(*numWrittenBits >= 8){
+            *numWrittenBits = 0;
+            fwrite(storedValue, 1, sizeof(char), stream);
+            storedValue = 0;
         }
     }
 }
