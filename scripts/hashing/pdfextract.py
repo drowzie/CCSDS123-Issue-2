39d2a52d7a608decb52031cc61613b9f80bce3d0
Christoffer Boothby <chrisboltern3@gmail.com>
2019-11-23 22:08:00 +0100

    code tables up to 11 is done, missing 12

--- scripts/hashing/pdfextract.py
+++ scripts/hashing/pdfextract.py
@@ -10,13 +10,16 @@ def generateConsZero(r):
     return txt
 
 # generate input code from low <= r <= high into rzeros + outputhex
-def generate(low, high, outputhex):
+#Return inputlist, outputcodes, and their bitsizes
+def generate(low, high, inputSuffix, outputhex, bitsize):
     inputList = []
     outputCode = []
+    bitSizes = []
     for x in range(low,high+1):
-        inputList.append(generateConsZero(x))
-        outputCode.append(bin(outputhex + int(hex(x),16))[:1:-1])
-    return inputList,outputCode
+        inputList.append(generateConsZero(x) + inputSuffix)
+        outputCode.append(hex(int(bin(outputhex + int(hex(x),16))[:1:-1],2)))
+        bitSizes.append(bitsize)
+    return inputList,outputCode, bitSizes
 
 def reverseHex(number):
     x = bin(int(number))[:1:-1]
@@ -35,26 +38,48 @@ def fullNumberFromString(txt):
 
 # Check for r in text
 def extractRFromString(txt):
-    x = txt.replace("}", "")
-    x = x.split(",")
-    number = x[0].split("^{")
+    x = txt.split(",")
+    lastWord = x[0].replace("0^{r}", "")
     minMax = x[1].split("≤r≤")
-    return number[0], minMax
-
+    return lastWord, [int(minMax[0]), int(minMax[1])]
 
 #Returns bitsize and hexvalue
 def extractOutputCode(outputTxt):
-    print(outputTxt)
     if("'" in outputTxt):
         txt = outputTxt.split("'h")
     else:
         txt = outputTxt.split("’h")
-    return txt[0], txt[1]
+    return txt[0], hex(int(txt[1], 16))
+
+# Extract hex value from eg. FFB from (FFB + r)
+def extractOutputHex(txt):
+    txt = txt.split("+")
+    txt[0] = txt[0].replace("(", "")
+    return int(txt[0],16)
 
 
-tabula.convert_into("a.pdf", "test.csv", output_format="csv", pages='90')
+# Extract numbits + outputhex for functoin generate
+def extractOutputIfReverse(outputTxt):
+    outputTxt = outputTxt.replace("<", "")
+    outputTxt = outputTxt.replace(">", "")
+    if("'" in outputTxt):
+        txt = outputTxt.split("'h")
+    else:
+        txt = outputTxt.split("’h")
+    return txt[0], extractOutputHex(txt[1])
 
-table = "10"
+test = "0^{r}X, 0≤r≤1"
+test2 = "<12'h(FFA+r)>"
+
+# input of format "0^{r}X, 0≤r≤1", output: "<12'h(FFA+r)>", remember to check for <>
+def generateInputOutputR(input, output):
+    suffixCode, rRange = extractRFromString(input)
+    bitsize, outputHex = extractOutputIfReverse(output)
+    return generate(rRange[0], rRange[1], suffixCode, outputHex, bitsize)
+
+tabula.convert_into("a.pdf", "test.csv", output_format="csv", pages='93')
+
+table = "12"
 
 with open('test.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -68,9 +93,7 @@ with open('test.csv', mode='r') as csv_file:
     outputCodes2 = []
 
     for enum, row in enumerate(csv_reader):
-        #if(row[0] == "Active Prefix"):
-        #    break
-        if(line_count > 1):
+        if(line_count > 13 and line_count < 33):
             print(row)
             inputCodes0.append(row[0])
             inputCodes1.append(row[2])
@@ -84,32 +107,45 @@ with open('test.csv', mode='r') as csv_file:
 inputList = inputCodes0 + inputCodes1 + inputCodes2
 outputList = outputCodes0 + outputCodes1 + outputCodes2
 
-file = open("codeTable"+table+".c", "w+")
-file.write("char codeTable" + table +"[][128] = {\r\n")
-for i in inputList:
-    #if("r" in i):
-
-    if("^" in i):
-        i = fullNumberFromString(i)
-    file.write('"'+i+'"'+",")
-file.write("\r\n};")
+#print(len(inputList))
+#print(outputList)
 
 bitsizes = []
 outPutValues = []
-for i in outputList:
-    if(not("'" in i or "’" in i)):
-        continue
-    bitsize, outputvalue = extractOutputCode(i)
-    bitsizes.append(bitsize)
-    outPutValues.append(outputvalue)
-file.write("uint32_t outPutCodes" + table +"[] = {\r\n")
-for i in outPutValues:
-    file.write('0x'+i+",")
-file.write("\r\n};")
-file.write("uint8_t bitSizes" + table +"[] = {\r\n")
-for i in bitsizes:
-    file.write(i+", ")
-file.write("\r\n};")
+inputcodes = []
+for i,o in zip(inputList, outputList):
+    if(i):
+        if("^{r}" in i):
+            inp, output, bitsize = generateInputOutputR(i,o)
+            inputcodes.extend(inp)
+            outPutValues.extend(output)
+            bitsizes.extend(bitsize)
+        elif("^" in i):
+            inCode = fullNumberFromString(i)
+            bitsize, outputvalue = extractOutputCode(o)
+            bitsizes.append(bitsize)
+            outPutValues.append(outputvalue)
+            inputcodes.append(inCode)
+        else:
+            bitsize, outputvalue = extractOutputCode(o)
+            outPutValues.append(outputvalue)
+            bitsizes.append(bitsize)
+            inputcodes.append(i)
 
+file = open("codeTable" + table + ".c", "w+")
+file.write("char codeTable"+ table +"[][128] = {\n")
+for i in inputcodes:
+    file.write('"'+ i + '"' + ", ")
+file.write("\n};\n")
+
+file.write("uint32_t outPutCodes" + table + "[] = {\n")
+for i in outPutValues:
+    file.write(i+", ")
+file.write("\n};\n")
+
+file.write("uint8_t bitSizes"+table+"[] = {\n")
+for i in bitsizes:
+    file.write(i + ", ")
+file.write("\n};\n")
 
 file.close()
