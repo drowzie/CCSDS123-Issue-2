c8d72572a40ddcf3dd91b1dfdef00677149942ea
Christoffer Boothby <chrisboltern3@gmail.com>
2020-02-20 09:37:22 +0100

    cleanup

--- scripts/hashing/flushTables/flushtable0.c
+++
@@ -1,9 +0,0 @@
-char activePrefix0[][128] = {
-"(null)", "0", "01", "02", "4", "44", "7", "8", 
-};
-uint32_t flushWord0[] = {
-0x0, 0x1, 0xf, 0x1f, 0x3, 0x3f, 0x7, 0x17, 
-};
-uint8_t bitSizeFlush0[] = {
-1, 2, 5, 6, 3, 6, 5, 5, 
-};
